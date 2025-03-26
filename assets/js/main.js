41b7ec054699ca59a528aea98c9d9d4da7c6b719
freetimezp <kraftukrnet@ukr.net>
2025-03-26 21:30:35 +0200

    02. usefull-things-094-text-reveal. UI. Reveal animation.

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+
+const copies = document.querySelectorAll(".copy");
+
+function setupSplite() {
+    $(document).ready(function () {
+        $(".split-line").wrap('<div class="split-parent"></div>');
+    });
+
+    copies.forEach((copy) => {
+        copy.split = new SplitText(copy, {
+            type: 'lines, words, chars',
+            linesClass: "split-line"
+        });
+
+        copy.anim = gsap.from(copy.split.lines, {
+            scrollTrigger: {
+                trigger: copy,
+                toggleAction: "restart pause resume",
+                start: "top 90%"
+            },
+            duration: 2,
+            ease: "power4.inOut",
+            yPercent: 100,
+            stagger: 0.1
+        });
+    });
+}
+
+setupSplite();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
